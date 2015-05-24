93ffda6d9d3ac6929e247b3b655cedcdf7eaf569
jiangxiao <jiangxiao89@qq.com>
2015-05-24 15:42:04 +0800

    添加了教师页面，下一步完成教师页面的详情

---
+++ src/com/idroid/eteach/adapter/TeacherAdapter.java
@@ -0,0 +1,32 @@
+package com.idroid.eteach.adapter;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import android.widget.AbsListView;
+
+import com.idroid.eteach.R;
+import com.idroid.eteach.adapter.internal.AdapterViewHolder;
+import com.idroid.eteach.adapter.internal.KJAdapter;
+
+public class TeacherAdapter extends KJAdapter<HashMap<String, Object>> {
+
+	public TeacherAdapter(AbsListView listView,
+			Collection<HashMap<String, Object>> datas, int itemLayoutId) {
+		super(listView, datas, itemLayoutId);
+	}
+
+	@Override
+	public void convert(AdapterViewHolder helper, HashMap<String, Object> item,
+			boolean isScrolling) {
+		helper.setText(R.id.itv_teacher_name, "宋宇");
+		helper.setText(R.id.itv_teacher_answ, "23回复");
+		helper.setText(R.id.itv_teacher_date, "2015-05-20");
+		
+		helper.setText(R.id.itv_teacher_info, "我周六上午有空，有需要补习数学、物理、化学的同学可以联系我哦~");
+		helper.setText(R.id.itv_teacher_time, "时间：2015-05-22 上午09:00");
+		helper.setText(R.id.itv_teacher_addr, "地点：重庆邮电大学");
+		helper.setText(R.id.itv_teacher_class, "科目：数学 | 物理 | 化学");
+	}
+
+}
--- src/com/idroid/eteach/ui/BaseActivityDemo.java
+++ src/com/idroid/eteach/ui/BaseActivityDemo.java
@@ -10,6 +10,7 @@ import com.idroid.eteach.R;
 import com.idroid.eteach.adapter.FragmentPagerAdapterDemo;
 import com.idroid.eteach.fragment.FragmentDemo;
 import com.idroid.eteach.fragment.MainHomeFragment;
+import com.idroid.eteach.fragment.TeacherFragment;
 import com.idroid.eteach.ui.base.ActivityFrame;
 
 public class BaseActivityDemo extends ActivityFrame{
@@ -37,7 +38,13 @@ public class BaseActivityDemo extends ActivityFrame{
 		list.add(m);
 		
 		
-		for (int i = 1; i < 4; i++) {
+		HashMap<String, Object> m2 = new HashMap<String, Object>();
+		Fragment v2= new TeacherFragment();
+		m2.put("content", v2);
+		m2.put("icon", TabIcons[1]);
+		list.add(m2);
+		
+		for (int i = 2; i < 4; i++) {
 			HashMap<String, Object> map = new HashMap<String, Object>();
 			Fragment v = new FragmentDemo(i);
 			map.put("content", v);
