a2cc23a3a9266f3341bd9071110cd478f89c3d50
jiangxiao <jiangxiao89@qq.com>
2015-06-02 19:48:26 +0800

    完成了新闻页面

---
+++ src/com/idroid/eteach/activity/NewsActivity.java
@@ -0,0 +1,41 @@
+package com.idroid.eteach.activity;
+
+import com.idroid.eteach.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class NewsActivity extends Activity {
+
+	private WebView webView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_news);
+		
+		init();
+	}
+
+	/**
+	 * 初始化数据
+	 */
+	private void init() {
+		webView = (WebView) this.findViewById(R.id.webView);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.loadUrl("http://cul.qq.com/a/20150601/009270.htm");
+		
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				view.loadUrl(url);
+				return true;
+			}
+		});
+	}
+	
+}
--- src/com/idroid/eteach/controller/MainHomeController.java
+++ src/com/idroid/eteach/controller/MainHomeController.java
@@ -3,6 +3,7 @@ package com.idroid.eteach.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import android.content.Intent;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,13 +16,14 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.idroid.eteach.R;
+import com.idroid.eteach.activity.NewsActivity;
 import com.idroid.eteach.adapter.MainHomeListViewAdapter;
 import com.idroid.eteach.adapter.internal.KJAdapter;
 import com.idroid.eteach.controller.base.BaseController;
 import com.idroid.eteach.fragment.MainHomeFragment;
 import com.idroid.eteach.fragment.base.FragmentBase;
 
-public class MainHomeController extends BaseController<FragmentBase> implements OnClickListener, OnItemClickListener{
+public class MainHomeController extends BaseController<FragmentBase> implements OnItemClickListener{
 	
 	public interface UI{
 		
@@ -66,19 +68,15 @@ public class MainHomeController extends BaseController<FragmentBase> implements 
 		}
 		
 		adapter = new MainHomeListViewAdapter(listView, list, resId);
-		adapter.setViewClickListener(R.id.item_like_img, this);
+//		adapter.setViewClickListener(R.id.item_like_img, this);
 		adapter.setItemClickListener(this);
 		((MainHomeFragment) ui).setListAdapter(adapter);
 	}
 
 	@Override
-	public void onClick(View v) {
-		Toast.makeText(getContext(), "like", 0).show();
-	}
-
-	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		Toast.makeText(getContext(), "item", 0).show();
+		Intent intent = new Intent(getContext(), NewsActivity.class);
+		getContext().startActivity(intent);
 	}
 	
 	
